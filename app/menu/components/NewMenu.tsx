45e01160e3c76f1ccb3e68e49598f416e5557b28
Davis Isibor <davisisibor@gmail.com>
2025-11-29 08:32:42 +0100

    update

--- app/menu/components/NewMenu.tsx
+++ app/menu/components/NewMenu.tsx
@@ -2,25 +2,35 @@
 
 import menu1 from "@/public/new_menu/menu1.webp";
 import menu2 from "@/public/new_menu/menu2.webp";
+import back_1 from "@/public/new_menu/back_1.webp";
+import back_2 from "@/public/new_menu/back_2.webp";
+import cover from "@/public/new_menu/cover.webp";
+import front_1 from "@/public/new_menu/front_1.webp";
+import front_2 from "@/public/new_menu/front_2.webp";
 import Image from "next/image";
+
 const NewMenu = () => {
+  const menuImages = [
+    { src: cover, alt: "Menu Cover" },
+    { src: front_1, alt: "Menu Front Page 1" },
+    { src: front_2, alt: "Menu Front Page 2" },
+
+    { src: back_1, alt: "Menu Back Page 1" },
+    { src: back_2, alt: "Menu Back Page 2" },
+  ];
+
   return (
     <>
-      <div className="md:w-[100rem] min-h-screen w-full mx-auto lg:flex-row justify-center lg:gap-8 flex flex-col items-center pb-20 pt-32  max-w-[96%]">
-        <div className="">
-          <Image
-            src={menu1}
-            alt="New Menu Page 1"
-            className="w-full  max-w-full h-auto mb-4 "
-          />
-        </div>
-        <div className="">
-          <Image
-            src={menu2}
-            alt="New Menu Page 2"
-            className="w-full  max-w-full h-auto mb-4 "
-          />
-        </div>
+      <div className="md:w-[100rem] min-h-40 w-full mx-auto lg:flex-row justify-center lg:gap-8 grid md:grid-cols-3 md:px-0 px-3  gap-7 items-center pb-20 pt-32 max-w-[96%]">
+        {menuImages.map((image, index) => (
+          <div key={index}>
+            <Image
+              src={image.src}
+              alt={image.alt}
+              className="w-full max-w-full h-auto "
+            />
+          </div>
+        ))}
       </div>
     </>
   );
